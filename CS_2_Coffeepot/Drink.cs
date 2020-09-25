db5017b9085eca862b1aa002b6a7b4dba7ebeb4f
Ilya Toritsyn <toritsyn41@gmail.com>
2020-09-25 09:55:28 +0300

    Добавьте файлы проекта.

---
+++ CS_2_Coffeepot/Drink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS_2_Coffeepot
+{
+    public class Drink
+    {
+        public Sugar Sugar
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ CS_2_Coffeepot/Sugar.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_2_Coffeepot
+{
+    public enum Sugar
+    {
+        Zero,
+        One,
+        Two,
+        Three
+    }
+}
